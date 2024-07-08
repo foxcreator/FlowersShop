0fae83f0ad6460f6c6f8a19a5cd5754c40de3fb0
foxcreator <sevastian.b@platinumlist.net>
2024-07-08 18:10:30 +0300

    fix cashier ix

--- config/checkbox.php
+++ config/checkbox.php
@@ -2,8 +2,8 @@
 
 return [
     'api_url' => env('CHECKBOX_API_URL', 'https://api.checkbox.ua/api/v1'),
-    'login' => env('CHECKBOX_LOGIN', 'test_osbpz1adaa4gn'),
-    'password' => env('CHECKBOX_PASSWORD', 'test_osbpzasasa14gn'),
-    'pincode' => env('CHECKBOX_PINCODE', '6532821asas621'),
-    'license_key' => env('CHECKBOX_LICENSE_KEY', 'test8ea51asasf0d691f792655848aa5'),
+    'login' => env('CHECKBOX_LOGIN', '1'),
+    'password' => env('CHECKBOX_PASSWORD', '1'),
+    'pincode' => env('CHECKBOX_PINCODE', '1'),
+    'license_key' => env('CHECKBOX_LICENSE_KEY', '1'),
 ];
