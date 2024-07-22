db6b5b00f6aa611d08747aca88cc82fa7d8b434a
foxcreator <sevastian.b@platinumlist.net>
2024-07-22 13:27:17 +0300

    add new map

--- resources/views/front/pages/contacts.blade.php
+++ resources/views/front/pages/contacts.blade.php
@@ -46,11 +46,10 @@
             </form>
         </div>
         <div class="contact-page__map">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2645.930566996194!2d35.04832811248104!3d48.45786172868895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbe2d916d55317%3A0xf916d0420635a754!2z0L_QtdGALiDQqNC10LLRh9C10L3QutC-LCA5LCDQlNC90LXQv9GALCDQlNC90LXQv9GA0L7Qv9C10YLRgNC-0LLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwgNDkwMDA!5e0!3m2!1sru!2sua!4v1718526505036!5m2!1sru!2sua"
-                width="100%" height="500"
-                style="border:0;" allowfullscreen=""
-                loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2806.501751754408!2d35.04610432045896!3d48.457945177530036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbe30366a61123%3A0x727bf3cebeea6580!2z0LzQsNCz0LDQt9C40L0g0LrQstGW0YLRltCyINGC0LAg0LTQtdC60L7RgNGDIFRoZUxvdHVzIGJvdXRpcXVl!5e0!3m2!1sru!2sua!4v1721643895218!5m2!1sru!2sua"
+                    width="100%" height="500"
+                    style="border:0;" allowfullscreen=""
+                    loading="lazy" referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
         </div>
     </div>
