c7089d91597a0dcb0759e10cf6809e4b5cbc4278
foxcreator <sevastian.b@platinumlist.net>
2024-12-26 13:17:50 +0200

    feat: fix working time

--- resources/views/front/layouts/cart-nav.blade.php
+++ resources/views/front/layouts/cart-nav.blade.php
@@ -9,7 +9,6 @@
     <div class="cart-nav__contacts">
         <a href="tel:0679776075" class="number">+38 067 977 60 75</a>
         <a href="tel:0732163409" class="number">+38 073 216 34 09</a>
-        <p class="worktime">пн-пт 8:30 – 20:00 <br>
-            сб-нд 9:00 – 20:00</p>
+        <p class="worktime">пн-нд 8:00 – 21:00</p>
     </div>
 </div>
